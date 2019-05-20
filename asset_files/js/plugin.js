64c2a0eb68c76f01a93183efdad7d6491f12de6a
Mohamed Khaled <m.khaled1499@gmail.com>
2019-05-20 14:31:03 +0200

    add error div to sign up page

--- asset_files/js/plugin.js
+++ asset_files/js/plugin.js
@@ -10,5 +10,20 @@ $(document).ready(function(){
     $( "#rightSide" ).load( "./asset_files/pages/signup.html" );
    });
    
-  
+   $("#loadValidation").click(function(e){
+            
+    e.preventDefault()  ; 
+    let mobile = $("#mobile").val() ; 
+    let age = $("#age").val() ; 
+    let gender = $('input[name=sex]:checked').val(); 
+
+    if (mobile && age && gender){
+        $( "#rightSide" ).load( "./asset_files/pages/validation.html" );
+    }else {
+        $(".alert-danger").css({
+            display: 'block',
+        }) ; 
+        $(".alert-danger").html("<p> Sorry but all Field must be filled") ; 
+    }
+})
 })
